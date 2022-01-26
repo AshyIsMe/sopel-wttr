15d41f8f074a6d9c5bc8ae711172ba368f5d30a6
Aaron Ash <aaron.ash@gmail.com>
2022-01-26 16:39:47 +1000

    Add .setlocation command

--- wttr.py
+++ wttr.py
@@ -18,10 +18,20 @@ def wttr(location):
 def sopel_wttr(bot, trigger):
     l = trigger.group(2)
     if l is None:
-        bot.reply("Specify a location: .wttr london")
-    else:
-        bot.reply(wttr(l))
+        l = bot.db.get_nick_value(trigger.nick, 'wttr_location')
+        if not l:
+            bot.reply("Specify a location: '.wttr london' or '.setlocation london'")
+            return
 
+    bot.reply(wttr(l))
+
+@sopel.module.commands("setlocation")
+def sopel_wttr_setlocation(bot, trigger):
+    l = trigger.group(2)
+    if l is None:
+        bot.reply("Specify a location: '.setlocation london'")
+    l = bot.db.set_nick_value(trigger.nick, 'wttr_location', l)
+    bot.reply("I now have you at: " + l)
 
 def main():
     # print( wttr('brisbane') )
